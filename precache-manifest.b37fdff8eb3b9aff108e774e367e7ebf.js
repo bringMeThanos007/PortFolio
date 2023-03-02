5e218f413249dd0f7cf71b298204df443c9fd4d4
amandalvi <82660417+amandalvi@users.noreply.github.com>
2023-03-02 22:31:09 +0530

    Updates

---
+++ precache-manifest.b37fdff8eb3b9aff108e774e367e7ebf.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "629a1a3197354c58e680",
+    "url": "/PortFolio/static/css/main.ecd5127b.chunk.css"
+  },
+  {
+    "revision": "629a1a3197354c58e680",
+    "url": "/PortFolio/static/js/main.629a1a31.chunk.js"
+  },
+  {
+    "revision": "5bff56686b744a6e6287",
+    "url": "/PortFolio/static/js/1.5bff5668.chunk.js"
+  },
+  {
+    "revision": "da75154d39791d72126f",
+    "url": "/PortFolio/static/js/2.da75154d.chunk.js"
+  },
+  {
+    "revision": "9e9791f7a1c8e3612ff1",
+    "url": "/PortFolio/static/js/runtime~main.9e9791f7.js"
+  },
+  {
+    "revision": "7a9d967c491116fe90e1752183712fa8",
+    "url": "/PortFolio/static/media/CV.7a9d967c.pdf"
+  },
+  {
+    "revision": "27bbda5fa349f4c79f4d6ffd07db612d",
+    "url": "/PortFolio/static/media/two.27bbda5f.png"
+  },
+  {
+    "revision": "b3257a4bf286ac7ec7892d07439a97a4",
+    "url": "/PortFolio/static/media/four.b3257a4b.png"
+  },
+  {
+    "revision": "835dd4827a927d19970954ca20918626",
+    "url": "/PortFolio/static/media/five.835dd482.png"
+  },
+  {
+    "revision": "f1bdeced27ef7fa81e327f12e035e6e0",
+    "url": "/PortFolio/static/media/sun.f1bdeced.png"
+  },
+  {
+    "revision": "35a6c0f3abdd97ba5074fdb59d80e4b9",
+    "url": "/PortFolio/static/media/cod.35a6c0f3.png"
+  },
+  {
+    "revision": "70b6fd8d36eb780e0dcaa8e0869030c1",
+    "url": "/PortFolio/static/media/sui.70b6fd8d.png"
+  },
+  {
+    "revision": "94658deb640c5be253b5e88d7955372b",
+    "url": "/PortFolio/static/media/code.94658deb.png"
+  },
+  {
+    "revision": "ed373fba2dddfcb5f257c98330defe7c",
+    "url": "/PortFolio/static/media/java.ed373fba.png"
+  },
+  {
+    "revision": "621f21fa891b48023ff7c4dff12c7aa0",
+    "url": "/PortFolio/static/media/css-3.621f21fa.png"
+  },
+  {
+    "revision": "d9de75e5bf3a7b0ef2a48321acc06e88",
+    "url": "/PortFolio/static/media/java-script.d9de75e5.png"
+  },
+  {
+    "revision": "1befcc4ce3ff6c0fcd919302622ca38c",
+    "url": "/PortFolio/static/media/php.1befcc4c.png"
+  },
+  {
+    "revision": "f3f62505de0766a4e362c7cb5df84bfb",
+    "url": "/PortFolio/static/media/android.f3f62505.png"
+  },
+  {
+    "revision": "6d91c2d39440a7868fa0c0003c95f428",
+    "url": "/PortFolio/static/media/react.6d91c2d3.png"
+  },
+  {
+    "revision": "5b9554e63b0436822fac7f80757fbb8b",
+    "url": "/PortFolio/static/media/node-js.5b9554e6.png"
+  },
+  {
+    "revision": "722938e0e4fb99f5b518095c0e3fd8f6",
+    "url": "/PortFolio/static/media/mern.722938e0.png"
+  },
+  {
+    "revision": "e4d536b1a07294afad43045def94294f",
+    "url": "/PortFolio/static/media/sql-server.e4d536b1.png"
+  },
+  {
+    "revision": "d21c30dcb302030b3e0cb6fa216d38fd",
+    "url": "/PortFolio/static/media/eight.d21c30dc.jpg"
+  },
+  {
+    "revision": "b154b536b7a827e67919326b6a4d7827",
+    "url": "/PortFolio/static/media/nine.b154b536.jpg"
+  },
+  {
+    "revision": "1e6d969bb126cbb42f86d9048f296899",
+    "url": "/PortFolio/static/media/ttt.1e6d969b.png"
+  },
+  {
+    "revision": "a83e7d9eed9b118ebd06bdbe36fda9d7",
+    "url": "/PortFolio/static/media/food_nation.a83e7d9e.png"
+  },
+  {
+    "revision": "56e69ca34d7f8a305eec56403182eef6",
+    "url": "/PortFolio/static/media/port.56e69ca3.png"
+  },
+  {
+    "revision": "017c1a9eb25b2dc61f25ccdf0c3f69f5",
+    "url": "/PortFolio/static/media/News_Nation.017c1a9e.png"
+  },
+  {
+    "revision": "818c9515a9c15f6d9eeffea4625c777e",
+    "url": "/PortFolio/static/media/qulab.818c9515.png"
+  },
+  {
+    "revision": "e14236b2ade0b2ba807a603b441eabc3",
+    "url": "/PortFolio/static/media/miwak.e14236b2.png"
+  },
+  {
+    "revision": "371e8902069eda1a2e98b7b10dc6a14e",
+    "url": "/PortFolio/static/media/trip.371e8902.png"
+  },
+  {
+    "revision": "36341c5e405c7c904ed1f189c26397b7",
+    "url": "/PortFolio/static/media/two.36341c5e.jpg"
+  },
+  {
+    "revision": "48154d97a4a67a792870da77204c8519",
+    "url": "/PortFolio/index.html"
+  }
+];
